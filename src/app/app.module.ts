4844ada9918c7eec2f711b5c4130cb1140dfe4c1
Michael Kidd <G00236920@gmit.ie>
2018-03-12 13:00:15 +0000

    MLogin Page added - No Functionality Yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NewPage } from '../pages/new/new';
 import { SendPage } from '../pages/send/send';
 import { AccountsPage } from '../pages/accounts/accounts';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
+    LoginPage,
     MyApp,
     NewPage,
     SendPage,
@@ -25,6 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    LoginPage,
     MyApp,
     NewPage,
     SendPage,
