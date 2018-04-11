32308fa7220874c7ba663ec9d93d916f09e32748
G00236920 <g00236920@gmit.ie>
2018-04-11 20:08:12 +0100

    some commenting added

--- src/pages/account-details/account-details.ts
+++ src/pages/account-details/account-details.ts
@@ -24,18 +24,28 @@ export class AccountDetailsPage {
   
   itemsList: any;
 
+  //Boolean to switch between the credit and debitson the account
+  //Later i would switch this to card
   showDebit: boolean = true;
   showCredit: boolean = false;
+
+  //Title that will be shown on the button
   titleButton = "Show Credit";
+
+  //Variable to be passed to the firebase provider to show if debit or credit
   state = "debited";
+
+  //The account balance
   bal: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public titleName: TitleProvider, public afd: FirebaseServiceProvider, public CurrentImage: ImageServiceProvider) {
   
     this.title = this.titleName.getAccount();
 
+    //Connect to firebase and retrieve the balance on the account
     this.afd.getBalance(this.title).then(snap=>{
 
+      //Find the child of the account with the name balance and set this.bal to it
       this.bal = snap.child("balance").val();
 
       return this.bal;
@@ -60,15 +70,18 @@ export class AccountDetailsPage {
 
   showDocument(image: any){
 
+    //Used to set the image that will be shown when the user requests to see the document for an entry
     this.CurrentImage.setPhoto(image);
+    //Push ShowPhotoPage to the stack, showing the page
     this.navCtrl.push(ShowPhotoPage);
     
   }
 
   creditAccount(){
     
+    //POP all pages off the stack until root is found
     this.navCtrl.popToRoot().then(value => {
-
+      //then push the addEntryPage to the stack
       this.navCtrl.push(AddEntryPage);
 
     });
@@ -77,6 +90,8 @@ export class AccountDetailsPage {
 
   swap(){
 
+    //Switch between the debit and credit of an account
+    //Set the button values and states, which are used when we call functions from the firebase provider
     if(this.showDebit == true){
 
       this.showDebit = false;
@@ -89,6 +104,7 @@ export class AccountDetailsPage {
     }
     else{
 
+      //Switch to the credit side
       this.showDebit = true;
       this.showCredit = false;
 
@@ -98,6 +114,7 @@ export class AccountDetailsPage {
 
     }
     
+    //set the list of items, this will allow them to be printed to screen
     this.itemsList = this.afd.getDetails(this.title, this.state);
 
   }
--- src/pages/accounts/accounts.ts
+++ src/pages/accounts/accounts.ts
@@ -10,24 +10,30 @@ import { TitleProvider } from '../../providers/title/title'
 })
 export class AccountsPage {
 
+  //All accounts
   accounts: any;
+  //The account currently being used
   currentAccount: any;
 
   constructor(public navCtrl: NavController, public afd: FirebaseServiceProvider, private titleName: TitleProvider) {
 
   }
 
+
   ngOnInit(){
     
+    //Get all the accounts for the current user
     this.accounts = this.afd.getAccounts();
     
   }
 
+
   accountEdit(account: string){
 
+    //set the title of the current account
     this.titleName.setAccount(account);
 
-    
+    //Push the AccountDetailsPage to navigation
     this.navCtrl.push( AccountDetailsPage );
 
   }
