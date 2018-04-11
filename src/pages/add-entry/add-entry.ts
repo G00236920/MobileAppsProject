e1fe0b300220e8a52e049e58e0e80c984ca60c0c
G00236920 <g00236920@gmit.ie>
2018-04-11 09:45:20 +0100

    changed photo type to PNG and Quality to half the previous setting, this should allow firebase to store the image as a string

--- src/pages/add-entry/add-entry.ts
+++ src/pages/add-entry/add-entry.ts
@@ -27,9 +27,9 @@ export class AddEntryPage {
   base64Image: string;
 
   options: CameraOptions = {
-    quality: 100,
+    quality: 50,
     destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
+    encodingType: this.camera.EncodingType.PNG,
     mediaType: this.camera.MediaType.PICTURE
   }
 
